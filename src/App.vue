558e753a3cd47bd466f6a3b813b96571d0ebaad6
Maylson <maylsonmoraesd@hotmail.com>
2022-01-05 11:43:20 -0300

    componentes de repetição do projeto

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+<Navbar :logo="logo_src" :alt="app_name"/>
+  <router-view/>
+<Footer />
+</template>
+<script>
+import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
+export default {
+  name:"App",
+  components: {
+    Navbar,
+    Footer
+  },
+  data() {
+    return {
+      logo_src:"./img/logo.png",
+      app_name:"Make Your Burguer"
+    }
+  }
+}
+</script>
+<style>
+ * {
+   font-family: helvetica;
+   padding: 0px;
+   margin: 0px;
+   box-sizing: border-box;
+ }
+
+ .main-container {
+   margin: 50px;
+   min-height: 250px;
+ }
+ .home {
+   margin: 50px;
+   min-height: 250px;
+ }
+
+ h1 {
+   text-align: center;
+   font-size: 42px;
+   margin-bottom: 30px;
+   color: #222;
+ }
+</style>
